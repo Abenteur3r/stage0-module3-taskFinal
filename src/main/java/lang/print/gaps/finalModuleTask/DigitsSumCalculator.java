62e34744959b761728b7d68697cd10b75705889a
Ayan <ajan.kohan@gmail.com>
2023-05-26 01:58:01 +0600

    M3 final-task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public void calculateSum(int number) {// 4891
+        int d = number%10; //4891 - 1
+        int c = (number%100) - d; //91 - 90
+        int b = (number%1000) - c -d; //891 - 800
+        int a = (number-b-c-d);
+        c /=10;
+        b /=100;
+        a /=1000;
+        System.out.println(a+b+c+d);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int c = number%10; //489 - 9
+        int b = ((number%100) - c)/10; //89 - 8
+        int a = (number-(number%100))/100;//400 - 4
+        System.out.println(c+""+b+""+a);
+
     }
 }
